1c7c6f5b50a75921a996bbd9f2735e421744726a
igctty <tatsu.smartphone@gmail.com>
2020-09-10 20:30:37 +0900

    feat: 枠を描画するようにした

--- src/camera.py
+++ src/camera.py
@@ -5,6 +5,8 @@ import numpy as np
 from PIL import ImageFont, ImageDraw, Image
 import glob
 
+face_locations = []
+
 # 入力
 video_capture = cv2.VideoCapture(0)
 
@@ -12,6 +14,15 @@ def camera():
   while True:
     # ビデオの単一フレームを取得
     _, frame = video_capture.read()
+    
+    # 顔の位置情報検索
+    face_locations = face_recognition.face_locations(frame)
+
+    # 位置情報の表示
+    for (top, right, bottom, left) in face_locations:
+      # 枠を描画
+      cv2.rectangle(frame, (left, top), (right, bottom), (0,0,255), 3)
+
     # 結果をビデオに表示
     cv2.imshow('Video', frame)
 
